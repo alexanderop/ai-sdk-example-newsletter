8aeb013803fdf7503e3c6a1eda6d70493939fbb7
Alexander Opalic <opalic@gmx.net>
2025-11-09 10:45:14 +0100

    test: add Hacker News stories factory

---
+++ tests/factories/hn-stories.factory.ts
@@ -0,0 +1,43 @@
+import { faker } from '@faker-js/faker'
+import { HNSearchResponseSchema, type HNSearchResponse, type HNStory } from '../schemas/hn.schema'
+
+interface HNResponseOptions {
+  hitCount?: number
+  daysOld?: number
+  minPoints?: number
+}
+
+export function createHNStory(daysOld: number = 0, minPoints: number = 20): HNStory {
+  const createdAt = new Date()
+  createdAt.setDate(createdAt.getDate() - daysOld)
+
+  return {
+    objectID: faker.string.numeric(8),
+    title: faker.lorem.sentence(),
+    url: faker.internet.url(),
+    points: faker.number.int({ min: minPoints, max: 500 }),
+    num_comments: faker.number.int({ min: 0, max: 100 }),
+    author: faker.internet.username(),
+    created_at: createdAt.toISOString()
+  }
+}
+
+export function createHNResponse(options: HNResponseOptions = {}): HNSearchResponse {
+  const {
+    hitCount = 5,
+    daysOld = 0,
+    minPoints = 20
+  } = options
+
+  const hits = Array.from({ length: hitCount }, () => createHNStory(daysOld, minPoints))
+
+  const response = {
+    hits,
+    nbHits: hitCount,
+    page: 0,
+    nbPages: 1,
+    hitsPerPage: 20
+  }
+
+  return HNSearchResponseSchema.parse(response)
+}
